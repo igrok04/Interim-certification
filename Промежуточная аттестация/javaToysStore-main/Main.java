71b2b50a3aa44a13277dbfdb43e7c77e1d49de67
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-04-10 00:09:34 +0300

    Add files via upload

---
+++ Промежуточная аттестация/javaToysStore-main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    Menu menu;
+
+    public static void main(1String[] args) {
+        //инициализация объекта меню
+        Menu menu = new Menu();
+        menu.run();
+    }
+}
